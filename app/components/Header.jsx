7896e808f004f1fb4810e47a9cc0bbfec3f6f413
Lawani-EJ <elyonjohn60@gmail.com>
2025-01-20 01:42:33 +0100

    feat(auth): Add client-side rendered Header component with user authentication

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,26 +1,34 @@
+"use client";
+
 import Link from 'next/link';
-import { UserButton } from '@clerk/nextjs';
+import { UserButton, useAuth } from '@clerk/nextjs';
 
 const Header = () => {
+  const { userId } = useAuth();
+
   return (
     <nav className='flex items-center justify-between px-6 py-4 mb-5 bg-blue-700'>
       <div className='flex items-center'>
         <Link href='/'>
-          <div className='text-lg font-bold text-white uppercase'>
-            Clerk App
-          </div>
+          <div className='text-lg font-bold text-white uppercase'>Clerk App</div>
         </Link>
       </div>
       <div className='flex items-center text-white'>
-        <Link href='/sign-in' className='text-gray-300 hover:text-white mr-4'>
-          Sign In
-        </Link>
-        <Link href='/sign-up' className='text-gray-300 hover:text-white mr-4'>
-          Sign Up
-        </Link>
-        <Link href='/profile' className='text-gray-300 hover:text-white mr-4'>
-          Profile
-        </Link>
+        {!userId && (
+          <>
+            <Link href='sign-in' className='text-gray-300 hover:text-white mr-4'>
+              Sign In
+            </Link>
+            <Link href='sign-up' className='text-gray-300 hover:text-white mr-4'>
+              Sign Up
+            </Link>
+          </>
+        )}
+        {userId && (
+          <Link href='profile' className='text-gray-300 hover:text-white mr-4'>
+            Profile
+          </Link>
+        )}
         <div className='ml-auto'>
           <UserButton afterSignOutUrl='/' />
         </div>
