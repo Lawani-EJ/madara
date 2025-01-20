18600a967de5b577d5726927b13931862c251035
Lawani-EJ <elyonjohn60@gmail.com>
2025-01-20 01:44:08 +0100

    feat(auth): Added the initial homepage content

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <h1 className='text-2xl font-bold mb-5'>Welcome</h1>
+    <p className="mb-5">
+      This is a demo for the clerk tutorial 
+      Go ahead and sign in or sign up
+    </p>
+    </>
   );
 }
